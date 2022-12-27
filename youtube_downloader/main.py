2017a19244146e6bee35afac4b59f960ce717db2
danieljkpraveen <danieljkpraveen@gmail.com>
2022-12-27 08:53:32 +0530

    cli tool to download youtube videos

--- youtube_downloader/main.py
+++ youtube_downloader/main.py
@@ -1,4 +1,5 @@
-from pytube import YouTube
+import re
+from pytube import YouTube, Playlist
 
 
 def download_video(link):
@@ -24,20 +25,33 @@ def download_audio(link, name):
         print("\n❎ Download Failed\n")
     
 
+def download_playlist(link):
+    download_path = 'D:\\youtube_download'
+    playlist = Playlist(link)
+    playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
+    for video in playlist.videos:
+        video.streams.\
+        filter(type='video', progressive=True, file_extension='mp4').\
+        order_by('resolution').\
+        desc().\
+        first().\
+        download(download_path)
 
 
 if __name__ == '__main__':
     link = input("Enter link ▶ ")
     choice = True
     while choice:
-        option = int(input("1. Download Video\n2. Download Audio\n3. Quit\nEnter Choice ▶ "))
+        option = int(input("1. Download Video\n2. Download Audio\n3. Download Playlist\n4. Quit\nEnter Choice ▶ "))
         if (option == 1):
             download_video(link)
         elif (option == 2):
             name = input("Enter Filename ▶ ")
             download_audio(link, name)
         elif (option == 3):
-            choice = False
+            download_playlist(link)
+        elif (option == 4):
+            choice = Falsee
         else:
             print("Invalid Option")
 
